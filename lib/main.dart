dc20de1eb1b8d258ab406d9c140721445d853c78
ramonzaccaron <ramonzaccaron@hotmail.com>
2019-09-29 21:35:41 -0300

    Desenvolvimento de um aplicativo de financas basenado-se no curso do Alura.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bytebank/screens/transferencias/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+      ),
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/transferencias/formulario.dart
@@ -0,0 +1,64 @@
+import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/transferencia.dart';
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'Criando Transferência';
+const _rotuloCampoValor = 'Valor';
+const _dicaCampoValor = '0.00';
+const _rotuloCamponumeroConta = 'Número da conta';
+const _dicaCampoNumeroConta = '0000';
+const _textoBotaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return FormularioTransferenciaState();
+  }
+}
+
+class FormularioTransferenciaState extends State<FormularioTransferencia> {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  void _criaTransferencia(BuildContext context) {
+    final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double valor = double.tryParse(_controladorCampoValor.text);
+
+    if (numeroConta != null && valor != null) {
+      final Transferencia transferenciaCriada =
+          Transferencia(valor, numeroConta);
+
+      Navigator.pop(context, transferenciaCriada);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(_tituloAppBar),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Editor(
+                  controlador: _controladorCampoNumeroConta,
+                  rotulo: _rotuloCamponumeroConta,
+                  dica: _dicaCampoNumeroConta),
+              Editor(
+                  controlador: _controladorCampoValor,
+                  rotulo: _rotuloCampoValor,
+                  dica: _dicaCampoValor,
+                  icone: Icons.monetization_on),
+              RaisedButton(
+                onPressed: () => _criaTransferencia(context),
+                child: Text(_textoBotaoConfirmar),
+              ),
+            ],
+          ),
+        ));
+  }
+}
